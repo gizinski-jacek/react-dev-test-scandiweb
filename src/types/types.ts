2af4159a468c3c71569221a4c6f3bba8b5146a1a
Jacek Gizinski <jacektrg+github@gmail.com>
2022-11-01 00:17:32 +0100

    Adding types for graphql data.

---
+++ src/types/types.ts
@@ -0,0 +1,39 @@
+export interface Price {
+	currency: Currency;
+	amount: number;
+}
+
+export interface Attribute {
+	displayValue: string;
+	value: string;
+	id: string;
+}
+
+export interface AttributeSet {
+	id: string;
+	name: string;
+	type: string;
+	items: Attribute[];
+}
+
+export interface Product {
+	id: string;
+	name: string;
+	inStock: boolean;
+	gallery: string[];
+	description: string;
+	category: string;
+	attributes: AttributeSet[];
+	prices: Price[];
+	brand: string;
+}
+
+export interface Category {
+	name: string;
+	products: Product[];
+}
+
+export interface Currency {
+	label: string;
+	symbol: string;
+}
