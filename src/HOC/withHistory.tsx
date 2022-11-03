3d3faeac15e27452601403c59a3f33a0b1a3f9ed
Jacek Gizinski <jacektrg+github@gmail.com>
2022-11-03 23:33:00 +0100

    Merging HOC's into single one.

--- src/HOC/withHistory.tsx
+++
@@ -1,9 +0,0 @@
-import { createBrowserHistory } from 'history';
-
-const withHistory = (WrappedComponent: any) => (props: any) => {
-	const history = createBrowserHistory();
-
-	return <WrappedComponent {...props} history={history} />;
-};
-
-export default withHistory;
--- src/HOC/withParams.tsx
+++
@@ -1,9 +0,0 @@
-import { useParams } from 'react-router-dom';
-
-const withParams = (WrappedComponent: any) => (props: any) => {
-	const params = useParams();
-
-	return <WrappedComponent {...props} params={params} />;
-};
-
-export default withParams;
