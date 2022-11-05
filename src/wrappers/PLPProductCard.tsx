099e099c83c02d3ee3237e85e871be6107956adb
Jacek Gizinski <jacektrg+github@gmail.com>
2022-11-05 18:21:50 +0100

    Styling out of stock products.
    Changing product Card to link to product's page.
    Adjusting styling of Details

--- src/wrappers/PLPProductCard.tsx
+++ src/wrappers/PLPProductCard.tsx
@@ -10,18 +10,48 @@ import { AppDispatch } from '../app/store';
 import { addItem } from '../features/cartSlice';
 import { connect } from 'react-redux';
 import { nanoid } from 'nanoid';
+import { Link } from 'react-router-dom';
 
-const Card = styled.div`
-	margin: 2rem;
+const Card = styled(Link)`
+	padding: 1rem;
 	display: flex;
 	flex-direction: column;
-	max-width: 300px;
+	transition: 0.25s ease-in-out;
+	text-decoration: none;
+	color: #000000;
+
+	&:hover {
+		box-shadow: 0 0 1rem 0.25rem #00000040;
+
+		div {
+			opacity: 1;
+		}
+	}
 `;
 
-const ImageWrapper = styled.div`
+const ImageWrapper = styled.div.attrs((props: { inStock: boolean }) => ({
+	inStock: props.inStock ? '' : '',
+}))`
+	max-width: 280px;
+	height: 280px;
 	position: relative;
-	width: 300px;
-	height: 300px;
+
+	&.out-of-stock {
+		img {
+			opacity: 0.25;
+		}
+
+		:before {
+			font-size: 1.5rem;
+			white-space: nowrap;
+			content: 'Out of Stock';
+			text-transform: uppercase;
+			position: absolute;
+			top: 50%;
+			left: 50%;
+			transform: translate(-50%, -50%);
+		}
+	}
 `;
 
 const Image = styled.img`
@@ -31,6 +61,7 @@ const Image = styled.img`
 `;
 
 const CartIcon = styled.div`
+	opacity: 0;
 	display: block;
 	position: absolute;
 	right: 8px;
@@ -44,12 +75,15 @@ const CartIcon = styled.div`
 
 	&:hover {
 		transform: scale(1.25);
-		background-color: #00c100;
 	}
 `;
 
 const Details = styled.div`
-	margin: 1rem 0;
+	flex: 1;
+	margin-top: 1rem;
+	display: flex;
+	flex-direction: column;
+	justify-content: space-between;
 `;
 
 interface Props {
@@ -84,18 +118,25 @@ class PLPProductCard extends Component<Props> {
 			(price) => price.currency.label === this.props.selectedCurrency.label
 		);
 		return (
-			<Card key={this.props.product.name}>
-				<ImageWrapper>
+			<Card
+				key={this.props.product.name}
+				to={`/product/${this.props.product.id}`}
+			>
+				<ImageWrapper
+					className={`${!this.props.product.inStock ? 'out-of-stock' : ''}`}
+				>
 					<Image src={this.props.product.gallery[0]} alt='' />
-					<CartIcon onClick={() => this.add(this.props.product)}>
-						<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 512 512'>
-							<path
-								fill='#ffffff'
-								d='M351.9 329.506H206.81l-3.072-12.56H368.16l26.63-116.019-217.23-26.04-9.952-58.09h-50.4v21.946h31.894l35.233 191.246a32.927 32.927 0 1 0 36.363 21.462h100.244a32.825 32.825 0 1 0 30.957-21.945zM181.427 197.45l186.51 22.358-17.258 75.195H198.917z'
-								data-name='Shopping Cart'
-							/>
-						</svg>
-					</CartIcon>
+					{this.props.product.inStock && (
+						<CartIcon onClick={() => this.add(this.props.product)}>
+							<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 512 512'>
+								<path
+									fill='#ffffff'
+									d='M351.9 329.506H206.81l-3.072-12.56H368.16l26.63-116.019-217.23-26.04-9.952-58.09h-50.4v21.946h31.894l35.233 191.246a32.927 32.927 0 1 0 36.363 21.462h100.244a32.825 32.825 0 1 0 30.957-21.945zM181.427 197.45l186.51 22.358-17.258 75.195H198.917z'
+									data-name='Shopping Cart'
+								/>
+							</svg>
+						</CartIcon>
+					)}
 				</ImageWrapper>
 				<Details>
 					<h4>
