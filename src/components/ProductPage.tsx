f226a55d80502195f4f2dadd4ec76cefdcc6f5c8
Jacek Gizinski <jacektrg+github@gmail.com>
2022-11-07 00:39:26 +0100

    Adding changeAttribute handler
    to conform to test requirements of being able to change product
    attributes only on product page and not in carts.
    Importing redux state reducer, updating props.
    Adding addToCart handler
    Passing onClick handlers to ProductAttributes and add to cart Button.

--- src/components/ProductPage.tsx
+++ src/components/ProductPage.tsx
@@ -4,29 +4,54 @@ import { connect } from 'react-redux';
 import styled from 'styled-components';
 import { client } from '..';
 import { AppDispatch } from '../app/store';
-import { addItem } from '../features/cartSlice';
+import { addItem, changeItemAttribute } from '../features/cartSlice';
 import withRouter from '../HOC/withRouter';
 import Button from '../reusables/Button';
-import { CartProduct, Currency, WithRouter } from '../types/types';
+import {
+	Attribute,
+	AttributeSet,
+	CartProduct,
+	Currency,
+	WithRouter,
+} from '../types/types';
 import productToCartProduct from '../utils/productToCartProduct';
 import Image from '../reusables/Image';
 import ProductAttributes from '../wrappers/ProductAttributes';
 
 const Product = styled.div`
 	display: flex;
+	justify-content: space-evenly;
 `;
 
 const Gallery = styled.div`
 	display: flex;
-	flex-direction: column;
+
+	> div {
+		display: flex;
+		flex-direction: column;
+		flex-wrap: wrap;
+		gap: 1rem;
+		max-height: 600px;
+		margin-right: 1rem;
+	}
 `;
 
-const Info = styled.div``;
+const Info = styled.div`
+	> * {
+		margin: 1rem;
+	}
+
+	span {
+		display: block;
+		font-weight: 600;
+	}
+`;
 
 interface Props {
 	withRouter: WithRouter;
 	selectedCurrency: Currency;
 	addItem: (product: CartProduct) => void;
+	changeItemAttribute: (product: CartProduct) => void;
 }
 
 interface State {
@@ -59,31 +84,72 @@ class ProductPage extends Component<Props> {
 		});
 	};
 
+	changeAttribute = (
+		e: React.MouseEvent<HTMLDivElement>,
+		attribute: AttributeSet,
+		attributeItem: Attribute
+	) => {
+		e.stopPropagation();
+		if (!this.state.product) return;
+		const updatedAttribute = this.state.product.selectedAttributes.map((att) =>
+			att.id === attribute.id ? { ...attribute, item: attributeItem } : att
+		);
+		const updatedItem = {
+			...this.state.product,
+			selectedAttributes: updatedAttribute,
+		};
+		this.setState({ ...this.state, product: updatedItem });
+	};
+
+	addToCart = () => {
+		if (!this.state.product) return;
+		this.props.addItem(this.state.product);
+	};
+
 	render() {
+		const itemPrice = this.state.product?.prices.find(
+			(price) => price.currency.label === this.props.selectedCurrency.label
+		);
 		return (
 			this.state.product && (
 				<Product>
 					<Gallery>
-						{this.state.product.gallery.map((img, i) => (
-							<Image
-								key={i}
-								src={img}
-								mWidth={'80px'}
-								height={'80px'}
-								onClick={() => this.changeImage(i)}
-							/>
-						))}
+						<div>
+							{this.state.product.gallery.map((img, i) => (
+								<Image
+									key={i}
+									src={img}
+									width={'60px'}
+									height={'60px'}
+									cursor={'true'}
+									onClick={() => this.changeImage(i)}
+								/>
+							))}
+						</div>
+						<Image
+							src={this.state.activeImage}
+							width={'600px'}
+							height={'600px'}
+						/>
 					</Gallery>
-					<Image
-						src={this.state.activeImage}
-						mWidth={'280px'}
-						height={'280px'}
-					/>
 					<Info>
-						{this.state.product.brand}
-						{this.state.product.name}
-						<ProductAttributes product={this.state.product} />
-						<Button onClick={this.props.addItem}>Add to Cart</Button>
+						<h4 style={{ fontWeight: '600' }}>{this.state.product.brand}</h4>
+						<h4>{this.state.product.name}</h4>
+						<ProductAttributes
+							product={this.state.product}
+							onClick={this.changeAttribute}
+						/>
+						<span>Price:</span>
+						<h4 style={{ fontWeight: '600' }}>
+							{itemPrice?.currency.symbol}
+							{itemPrice?.amount}
+						</h4>
+						<Button
+							disabled={!this.state.product.inStock}
+							onClick={this.addToCart}
+						>
+							Add to Cart
+						</Button>
 					</Info>
 				</Product>
 			)
@@ -125,6 +191,8 @@ const GET_PRODUCT_DETAILS = gql`
 function mapDispatchToProps(dispatch: AppDispatch) {
 	return {
 		addItem: (product: CartProduct) => dispatch(addItem(product)),
+		changeItemAttribute: (product: CartProduct) =>
+			dispatch(changeItemAttribute(product)),
 	};
 }
 
