9fef5c8e1ea5250341cc6a7deb6b22a48351a15a
Jacek Gizinski <jacektrg+github@gmail.com>
2022-11-07 18:05:43 +0100

    Renaming props.

--- src/reusables/Image.tsx
+++ src/reusables/Image.tsx
@@ -2,13 +2,13 @@ import { Component } from 'react';
 import styled from 'styled-components';
 
 const ImgWrapper = styled.div<{
-	mWidth?: string;
+	maxWidth?: string;
 	width?: string;
 	height?: string;
 	inStock?: boolean;
 	cursor?: any;
 }>`
-	max-width: ${({ mWidth }) => mWidth || 'auto'};
+	max-width: ${({ maxWidth }) => maxWidth || 'auto'};
 	width: ${({ width }) => width || 'auto'};
 	height: ${({ height }) => height || 'auto'};
 	cursor: ${({ cursor }) => cursor === 'true' && 'pointer'};
@@ -44,7 +44,7 @@ interface Props {
 	children?: any;
 	src: string;
 	inStock?: boolean;
-	mWidth?: string;
+	maxWidth?: string;
 	width?: string;
 	height?: string;
 	cursor?: string;
