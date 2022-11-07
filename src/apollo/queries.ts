2e650a39ea5919d227cb538f54b65ab0b0e03936
Jacek Gizinski <jacektrg+github@gmail.com>
2022-11-07 15:45:58 +0100

    Moving gql queries to separate file.
    Updating initial data query.

---
+++ src/apollo/queries.ts
@@ -0,0 +1,73 @@
+import { gql } from '@apollo/client';
+
+export const GET_INITIAL_DATA = gql`
+	query {
+		categories {
+			name
+		}
+		currencies {
+			label
+			symbol
+		}
+	}
+`;
+
+export const GET_CATEGORY_PRODUCTS = gql`
+	query ($category: String!) {
+		category(input: { title: $category }) {
+			name
+			products {
+				id
+				name
+				brand
+				inStock
+				gallery
+				attributes {
+					id
+					name
+					type
+					items {
+						value
+						id
+					}
+				}
+				prices {
+					currency {
+						label
+						symbol
+					}
+					amount
+				}
+			}
+		}
+	}
+`;
+
+export const GET_PRODUCT_DETAILS = gql`
+	query Product($id: String!) {
+		product(id: $id) {
+			id
+			name
+			brand
+			inStock
+			gallery
+			description
+			attributes {
+				id
+				name
+				type
+				items {
+					value
+					id
+				}
+			}
+			prices {
+				currency {
+					label
+					symbol
+				}
+				amount
+			}
+		}
+	}
+`;
