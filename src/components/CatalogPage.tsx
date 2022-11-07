24a4e08306de022979592fe50c2bc886775caed6
Jacek Gizinski <jacektrg+github@gmail.com>
2022-11-07 15:46:28 +0100

    Moving catalog functionality to new component.

---
+++ src/components/CatalogPage.tsx
@@ -0,0 +1,112 @@
+import { Component } from 'react';
+import styled from 'styled-components';
+import { client } from '../apollo/client';
+import { GET_CATEGORY_PRODUCTS } from '../apollo/queries';
+import withRouter from '../HOC/withRouter';
+import {
+	CartProduct,
+	Currency,
+	GQLCategoryData,
+	WithRouter,
+} from '../types/types';
+import productToCartProduct from '../utils/productToCartProduct';
+import PLPProductCard from '../wrappers/PLPProductCard';
+
+const Catalog = styled.div`
+	h3 {
+		margin: 2rem 8rem;
+		text-transform: uppercase;
+	}
+
+	.plp-grid {
+		display: grid;
+		grid-template-columns: repeat(auto-fit, 280px);
+		grid-gap: 4rem;
+		justify-content: center;
+	}
+`;
+
+interface Props {
+	withRouter: WithRouter;
+	selectedCurrency: Currency;
+}
+
+interface State {
+	category: string;
+	productList: CartProduct[];
+}
+
+class CatalogPage extends Component<Props> {
+	state: State = {
+		category:
+			new URLSearchParams(this.props.withRouter.location.search).get(
+				'category'
+			) || 'all',
+		productList: [],
+	};
+
+	componentDidMount = async () => {
+		try {
+			const search = this.props.withRouter.location.search;
+			const category = new URLSearchParams(search).get('category') || 'all';
+			const response: GQLCategoryData = await client.query({
+				query: GET_CATEGORY_PRODUCTS,
+				variables: { category: category },
+			});
+			const cartProducts = response.data.category.products.map((product) =>
+				productToCartProduct(product)
+			);
+			this.setState({
+				...this.state,
+				category: response.data.category.name,
+				productList: cartProducts,
+			});
+		} catch (error) {
+			console.log(error);
+		}
+	};
+
+	componentDidUpdate = async (prevProps: Props, prevState: State) => {
+		try {
+			const search = this.props.withRouter.location.search;
+			const category = new URLSearchParams(search).get('category') || 'all';
+			if (category !== prevState.category) {
+				const response: GQLCategoryData = await client.query({
+					query: GET_CATEGORY_PRODUCTS,
+					variables: { category: category },
+				});
+				const cartProducts = response.data.category.products.map((product) =>
+					productToCartProduct(product)
+				);
+				this.setState({
+					...this.state,
+					category: response.data.category.name,
+					productList: cartProducts,
+				});
+			}
+		} catch (error) {
+			console.log(error);
+		}
+	};
+
+	render() {
+		const search = this.props.withRouter.location.search;
+		const category = new URLSearchParams(search).get('category');
+		return (
+			<Catalog>
+				<h3>{category === 'all' ? 'all products' : category}</h3>
+				<div className='plp-grid'>
+					{this.state.productList.map((product, i) => (
+						<PLPProductCard
+							key={i}
+							product={product}
+							selectedCurrency={this.props.selectedCurrency}
+						/>
+					))}
+				</div>
+			</Catalog>
+		);
+	}
+}
+
+export default withRouter(CatalogPage);
