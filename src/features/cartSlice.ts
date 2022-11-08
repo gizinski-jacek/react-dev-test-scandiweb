a409decd02da82ba70549322d9528a799ed074fe
Jacek Gizinski <jacektrg+github@gmail.com>
2022-11-08 17:54:03 +0100

    Minor refactoring of addProduct reducer.
    Updating types.

--- src/features/cartSlice.ts
+++ src/features/cartSlice.ts
@@ -1,43 +1,47 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
-import { CartProduct } from '../types/types';
+import { CartProductWithUID } from '../types/types';
 
-const initialState: CartProduct[] = [];
+const initialState: CartProductWithUID[] = [];
 
 export const cartSlice = createSlice({
 	name: 'cart',
 	initialState,
 	reducers: {
-		addProduct: (state, action: PayloadAction<CartProduct>) => {
+		addProduct: (state, action: PayloadAction<CartProductWithUID>) => {
 			const sameTypeProductsInCart = state.filter(
 				(product) => product.id === action.payload.id
 			);
-			const sameAttributesProduct = sameTypeProductsInCart?.find((product) => {
-				const sameAttributes = product.selectedAttributes.map(
-					(attribute) =>
-						action.payload.selectedAttributes.find(
-							(att) => att.id === attribute.id
-						)?.item.id === attribute.item.id
-				);
-				if (sameAttributes.every((v) => v)) {
-					return product;
-				} else return null;
-			});
-			if (sameAttributesProduct) {
-				const index = state.findIndex(
-					(product) => product.uid === sameAttributesProduct.uid
-				);
-				state[index].count++;
+			if (sameTypeProductsInCart.length) {
+				const sameAttributesProduct = sameTypeProductsInCart.find((product) => {
+					const sameAttributes = product.selectedAttributes.map(
+						(attribute) =>
+							action.payload.selectedAttributes.find(
+								(att) => att.id === attribute.id
+							)?.item.id === attribute.item.id
+					);
+					if (sameAttributes.every((v) => v)) {
+						return product;
+					} else return null;
+				});
+				if (sameAttributesProduct) {
+					const index = state.findIndex(
+						(product) => product.uid === sameAttributesProduct.uid
+					);
+					state[index].count++;
+				} else {
+					state.push(action.payload);
+				}
 			} else {
 				state.push(action.payload);
 			}
 		},
-		removeProduct: (state, action: PayloadAction<CartProduct>) => {
+		removeProduct: (state, action: PayloadAction<CartProductWithUID>) => {
 			const newState = state.filter(
 				(product) => product.uid !== action.payload.uid
 			);
 			return newState;
 		},
-		incrementProduct: (state, action: PayloadAction<CartProduct>) => {
+		incrementProduct: (state, action: PayloadAction<CartProductWithUID>) => {
 			const newState = state.map((product) =>
 				product.uid === action.payload.uid
 					? { ...product, count: product.count + 1 }
@@ -45,11 +49,12 @@ export const cartSlice = createSlice({
 			);
 			return newState;
 		},
-		decrementProduct: (state, action: PayloadAction<CartProduct>) => {
+		decrementProduct: (state, action: PayloadAction<CartProductWithUID>) => {
 			const productInCart = state.find(
 				(product) => product.id === action.payload.id
 			);
-			if (productInCart?.count === 1) {
+			if (!productInCart) return state;
+			if (productInCart.count === 1) {
 				const newState = state.filter(
 					(product) => product.uid !== action.payload.uid
 				);
@@ -63,7 +68,10 @@ export const cartSlice = createSlice({
 				return newState;
 			}
 		},
-		changeProductAttribute: (state, action: PayloadAction<CartProduct>) => {
+		changeProductAttribute: (
+			state,
+			action: PayloadAction<CartProductWithUID>
+		) => {
 			const newState = state.map((product) =>
 				product.uid === action.payload.uid ? action.payload : product
 			);
