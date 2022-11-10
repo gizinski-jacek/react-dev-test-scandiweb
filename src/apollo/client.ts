36f20e6a9e463fbf0bc5b84c4976ed0fdefd37fc
Jacek Gizinski <jacektrg+github@gmail.com>
2022-11-10 18:53:55 +0100

    Omitting __typename field from gql requests.
    Fixes issue with 'jacket' product having the same attributes for sizes
    as 'shoes' product (numbers) instead
    of its own sizes (letters S/M/etc).

--- src/apollo/client.ts
+++ src/apollo/client.ts
@@ -2,5 +2,5 @@ import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 export const client = new ApolloClient({
 	uri: 'http://localhost:4000/',
-	cache: new InMemoryCache(),
+	cache: new InMemoryCache({ addTypename: false }),
 });
