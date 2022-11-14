467ed3ac6da08f1141369c15006f19b2a02e6fa2
Jacek Gizinski <jacektrg+github@gmail.com>
2022-11-14 16:40:46 +0100

    Moving styling to separate component and importing

---
+++ src/styled/ProductAttributes.styled.tsx
@@ -0,0 +1,99 @@
+import styled from 'styled-components';
+
+export const Container = styled.div<{ bigger?: boolean }>`
+	display: flex;
+	flex-direction: column;
+	gap: 0.5rem;
+
+	${({ bigger }) =>
+		bigger &&
+		`
+		span {
+			font-size: 1.25rem;
+		}`};
+`;
+
+export const Attributes = styled.div`
+	> div {
+		display: flex;
+		flex-wrap: wrap;
+		gap: 0.5rem;
+		margin-top: 0.5rem;
+	}
+`;
+
+export const Color = styled.div<{
+	selected: boolean;
+	bgColor: string;
+	bigger?: boolean;
+}>`
+	width: ${({ bigger }) => (bigger ? '24px' : '16px')};
+	height: ${({ bigger }) => (bigger ? '24px' : '16px')};
+	border: 1px solid #a1a1a1;
+	cursor: pointer;
+	position: relative;
+	background-color: ${({ bgColor }) => bgColor || '#000000'};
+
+	${({ selected }) =>
+		selected &&
+		`
+		&:before {
+		border: 2px solid #64ff00;
+		content: '';
+		display: block;
+		position: absolute;
+		top: -4px;
+		left: -4px;
+		right: -4px;
+		bottom: -4px;
+	}
+  `}
+`;
+
+export const OtherAtt = styled.div<{
+	clickable?: boolean;
+	selected: boolean;
+	bigger?: boolean;
+}>`
+	width: ${({ bigger }) => (bigger ? '52px' : '24px')};
+	flex: 1;
+	display: inline-flex;
+	justify-content: center;
+	padding: ${({ bigger }) => (bigger ? '0.75rem 1rem' : '0.25rem 0.5rem')};
+	border: 1px solid #000000;
+	position: relative;
+
+	${({ clickable }) =>
+		clickable &&
+		`
+		cursor: pointer;
+		transition: 0.1s ease-in-out;
+
+		&:hover {
+			box-shadow: 0 0 1px 1px #960096;
+			border-color: #960096;
+		}
+
+		&:active {
+			box-shadow: 0 0 2px 2px #960096;
+			border-color: #960096;
+		}
+  `}
+
+	${({ selected }) =>
+		selected &&
+		`
+		background-color: #000000;
+		color: #ffffff;
+
+		&:hover {
+			border: 1px solid #000000;
+			box-shadow: none;
+		}
+
+		&:active {
+			border: 1px solid #000000;
+			box-shadow: none;
+		}
+  `}
+`;
--- src/wrappers/ProductAttributes.tsx
+++ src/wrappers/ProductAttributes.tsx
@@ -1,109 +1,11 @@
 import { Component } from 'react';
-import styled from 'styled-components';
+import * as styled from '../styled/ProductAttributes.styled';
 import {
 	Attribute,
 	AttributeSet,
 	CartProduct,
 	CartProductWithUID,
 } from '../types/types';
-
-const Container = styled.div<{ bigger?: boolean }>`
-	display: flex;
-	flex-direction: column;
-	gap: 0.5rem;
-
-	${({ bigger }) =>
-		bigger &&
-		`
-		span {
-			font-size: 1.25rem;
-		}`};
-`;
-
-const Attributes = styled.div`
-	> div {
-		display: flex;
-		flex-wrap: wrap;
-		gap: 0.5rem;
-		margin-top: 0.5rem;
-	}
-`;
-
-const Color = styled.div<{
-	selected: boolean;
-	bgColor: string;
-	bigger?: boolean;
-}>`
-	width: ${({ bigger }) => (bigger ? '24px' : '16px')};
-	height: ${({ bigger }) => (bigger ? '24px' : '16px')};
-	border: 1px solid #a1a1a1;
-	cursor: pointer;
-	position: relative;
-	background-color: ${({ bgColor }) => bgColor || '#000000'};
-
-	${({ selected }) =>
-		selected &&
-		`
-		&:before {
-		border: 2px solid #64ff00;
-		content: '';
-		display: block;
-		position: absolute;
-		top: -4px;
-		left: -4px;
-		right: -4px;
-		bottom: -4px;
-	}
-  `}
-`;
-
-const OtherAtt = styled.div<{
-	clickable?: boolean;
-	selected: boolean;
-	bigger?: boolean;
-}>`
-	width: ${({ bigger }) => (bigger ? '52px' : '24px')};
-	flex: 1;
-	display: inline-flex;
-	justify-content: center;
-	padding: ${({ bigger }) => (bigger ? '0.75rem 1rem' : '0.25rem 0.5rem')};
-	border: 1px solid #000000;
-	position: relative;
-
-	${({ clickable }) =>
-		clickable &&
-		`
-		cursor: pointer;
-		transition: 0.1s ease-in-out;
-
-		&:hover {
-			box-shadow: 0 0 1px 1px #960096;
-			border-color: #960096;
-		}
-
-		&:active {
-			box-shadow: 0 0 2px 2px #960096;
-			border-color: #960096;
-		}
-  `}
-
-	${({ selected }) =>
-		selected &&
-		`
-		background-color: #000000;
-		color: #ffffff;
-
-		&:hover {
-			border: 1px solid #000000;
-			box-shadow: none;
-		}
-
-		&:active {
-			border: 1px solid #000000;
-			box-shadow: none;
-		}
-  `}
-`;
 
 interface Props {
 	product: CartProductWithUID | CartProduct;
@@ -118,10 +20,10 @@ interface Props {
 class ProductAttributes extends Component<Props> {
 	render() {
 		return (
-			<Container bigger={this.props.bigger}>
+			<styled.Container bigger={this.props.bigger}>
 				{this.props.product.attributes?.map((att) => {
 					return (
-						<Attributes key={att.id}>
+						<styled.Attributes key={att.id}>
 							<span>{att.name}:</span>
 							<div>
 								{att.items.map((item) => {
@@ -131,7 +33,7 @@ class ProductAttributes extends Component<Props> {
 										)?.item.id === item.id;
 									if (att.type.toLowerCase() === 'swatch') {
 										return (
-											<Color
+											<styled.Color
 												key={item.value}
 												onClick={() =>
 													this.props.onClick && !selected
@@ -145,7 +47,7 @@ class ProductAttributes extends Component<Props> {
 										);
 									} else {
 										return (
-											<OtherAtt
+											<styled.OtherAtt
 												key={item.value}
 												onClick={() =>
 													this.props.onClick && !selected
@@ -157,15 +59,15 @@ class ProductAttributes extends Component<Props> {
 												bigger={this.props.bigger}
 											>
 												{item.value}
-											</OtherAtt>
+											</styled.OtherAtt>
 										);
 									}
 								})}
 							</div>
-						</Attributes>
+						</styled.Attributes>
 					);
 				})}
-			</Container>
+			</styled.Container>
 		);
 	}
 }
