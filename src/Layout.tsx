95daa3bb9dd141f376e01180e9d960cbe64fd226
Jacek Gizinski <jacektrg+github@gmail.com>
2022-11-23 23:36:23 +0100

    Removing props to reduce prop drilling

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,20 +1,12 @@
 import { Component } from 'react';
 import { Outlet } from 'react-router-dom';
 import Navbar from './components/Navbar';
-import { Category, Currency } from './types/types';
 
-interface NavbarProps {
-	categoryList: Category[];
-	changeCurrency: (currency: Currency) => void;
-	currencyList: Currency[];
-	selectedCurrency: Currency;
-}
-
-class Layout extends Component<NavbarProps> {
+class Layout extends Component {
 	render() {
 		return (
 			<>
-				<Navbar {...this.props} />
+				<Navbar />
 				<main>
 					<Outlet />
 				</main>
