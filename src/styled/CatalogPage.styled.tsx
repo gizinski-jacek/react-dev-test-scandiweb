8bc12677debe2786ce29c88967e2e76e57986e3b
Jacek Gizinski <jacektrg+github@gmail.com>
2022-11-23 23:53:28 +0100

    Changing text-transform to uppercase.
    Changing grid to 3 columns instead of auto-fit

--- src/styled/CatalogPage.styled.tsx
+++ src/styled/CatalogPage.styled.tsx
@@ -4,13 +4,13 @@ export const Catalog = styled.div`
 	h2 {
 		margin: 0 1rem;
 		margin-bottom: 4rem;
-		text-transform: capitalize;
+		text-transform: uppercase;
 	}
 `;
 
 export const Grid = styled.div`
 	display: grid;
-	grid-template-columns: repeat(auto-fit, 280px);
-	grid-gap: 4rem;
+	grid-template-columns: repeat(3, 320px);
+	grid-gap: 5rem;
 	justify-content: center;
 `;
